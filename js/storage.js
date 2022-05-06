b4335dc1c3d1066fdaad8ab61434a8f677766511
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-05-06 15:25:43 +0300

    ServiceLocalStorage

--- js/storage.js
+++ js/storage.js
@@ -1,3 +1,9 @@
+/**
+ *
+ * @param {*} key
+ * @param {*} value
+ */
+
 const save = (key, value) => {
   try {
     const serializedState = JSON.stringify(value);
@@ -7,10 +13,17 @@ const save = (key, value) => {
   }
 };
 
+/**
+ *
+ * @param {*} key
+ * @returns
+ */
 const load = key => {
   try {
     const serializedState = localStorage.getItem(key);
-    return serializedState === null ? undefined : JSON.parse(serializedState);
+    return serializedState === null
+    ? undefined
+    : JSON.parse(serializedState);
   } catch (error) {
     console.error("Get state error: ", error.message);
   }
