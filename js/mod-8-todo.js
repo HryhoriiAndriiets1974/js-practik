6d6c840a137e9fa42ca7f5f05f55890d0736400a
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-05-07 14:04:29 +0300

    +feedback_lms

--- js/mod-8-todo.js
+++ js/mod-8-todo.js
@@ -134,3 +134,19 @@ renderList();
 // person.refreshAge();
 
 // console.log(person.age);
+
+// =================== git
+
+// const date = new Date();
+
+// console.log(date);
+// // "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
+
+// console.log(date.toString());
+// // "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
+
+// console.log(typeof(date));
+// // "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
+
+// console.log(typeof(date.toString()));
+// // "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
