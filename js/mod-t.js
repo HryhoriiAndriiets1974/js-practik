74701c4e8220f0a79cb311d02f455c92b06fdac7
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-05-13 19:38:31 +0300

    mod-t+table-html

--- js/mod-t.js
+++ js/mod-t.js
@@ -193,12 +193,34 @@ const cars = [
       onSale: false,
     },
   ];
+
   console.table(cars);
+
+  const tableEl = document.querySelector('.table');
+
+  const itemTemplate = ({ make, model, type, amount, price, onSale }) => `
+  <tr>
+    <td> ${make} </td>
+    <td> ${model} </td>
+    <td> ${type} </td>
+    <td> ${amount} </td>
+    <td> ${price} </td>
+    <td> ${onSale} </td>
+  </tr>`;
+
+  const renderList = () => {
+    const list = cars.map(itemTemplate).join('');
+    tableEl.innerHTML = '';
+    tableEl.insertAdjacentHTML('beforeend', list);
+  };
+
+  renderList();
+
   // ====================================
   // Example 1 - Метод map
   // Пусть функция getModels возвращает массив
   //  моделей (поле model) всех автомобилей.
-  
+
   const getModels = (cars) => cars.map(({ model }) => model);
   console.table(getModels(cars));
   // ====================================
@@ -206,13 +228,13 @@ const cars = [
   // Пусть функция makeCarsWithDiscount возвращает
   //  новый массив объектов с изменным значением
   // свойства price в зависимости от переданной скидки.
-  
+
   const makeCarsWithDiscount = (cars, discount) =>
     cars.map((car) => ({
       ...car,
       price: car.price - car.price * discount,
     }));
-  
+
   console.table(makeCarsWithDiscount(cars, 0.2));
   console.table(makeCarsWithDiscount(cars, 0.4));
   console.table(makeCarsWithDiscount(cars, 0.9));
@@ -222,7 +244,7 @@ const cars = [
   // Пусть функция filterByPrice возвращает массив
   //  автомобилей цена которых меньше чем значение
   //  параметра threshold.
-  
+
   // const filterByPrice = (cars, threshold) =>
   //   cars.filter(({ price }) => price < threshold);
   // console.table(filterByPrice(cars, 30000));
@@ -231,7 +253,7 @@ const cars = [
   // Example 4 - Метод filter
   // Пусть функция getCarsWithDiscount возвращает
   //  массив автомобилей свойство onSale которых true.
-  
+
   // const getCarsWithDiscount = (cars) => cars.filter(({ onSale }) => onSale);
   // console.table(getCarsWithDiscount(cars));
   // ====================================
@@ -239,7 +261,7 @@ const cars = [
   // Пусть функция getCarsWithType возвращает массив
   //  автомобилей тип которых совпадает со значением
   //  параметра type.
-  
+
   // const getCarsWithType = (cars, carType) =>
   //   cars.filter(({ type }) => type === carType);
   // console.table(getCarsWithType(cars, 'suv'));
@@ -247,7 +269,7 @@ const cars = [
   // console.table(getCarsWithType(cars, 'truck'));
   // ====================================
   // Example 6 - Метод find
-  
+
   // const getCarByModel = (cars, carModel) =>
   //   cars.find(({ model }) => model === carModel);
   // console.log(getCarByModel(cars, 'F-150'));
@@ -258,19 +280,19 @@ const cars = [
   // Пусть функция sortByAscendingAmount возвращает
   //  новый массив автомобилей отсортированный по
   //  возврастанию значения свойства amount.
-  
+
   // const sortByAscendingAmount = (cars) =>
   //   [...cars].sort((a, b) => a.amount - b.amount);
   // console.table(sortByAscendingAmount(cars));
-  
+
   // const sortByDescendingAmount = (cars) =>
   //   [...cars].sort((a, b) => b.amount - a.amount);
   // console.table(sortByDescendingAmount(cars));
-  
+
   // const sortByAscendingPrice = (cars) =>
   //   [...cars].sort((a, b) => a.price - b.price);
   // console.table(sortByAscendingPrice(cars));
-  
+
   // const sortByDescendingPrice = (cars) =>
   //   [...cars].sort((a, b) => b.price - a.price);
   // console.table(sortByDescendingPrice(cars));
@@ -279,7 +301,7 @@ const cars = [
   // Пусть функция sortByDescendingPrice возвращает
   //  новый массив автомобилей отсортированный по
   // убыванию значения свойства price.
-  
+
   // const sortByDescendingPrice = (cars) =>
   //   [...cars].sort((a, b) => b.price - a.price);
   // console.table(sortByDescendingPrice(cars));
@@ -289,47 +311,47 @@ const cars = [
   //  автомобилей отсортированный по названию модели в
   //  алфавитном и обратном алфавитном порядке, в
   // засисимости от значения параметра order.
-  
+
   // const sortByModel = (cars, order) =>
   //   [...cars].sort((a, b) => {
   //     switch (order) {
   //       case 'asc':
   //         return a.model.localeCompare(b.model);
-  
+
   //       case 'desc':
   //         return b.model.localeCompare(a.model);
-  
+
   //       default:
   //         return 0;
   //     }
   //   });
-  
+
   // console.table(sortByModel(cars, 'asc'));
   // console.table(sortByModel(cars, 'desc'));
-  
+
   // сортувати в алф порядку по виробнику
-  
+
   // const sortByMake = (cars, order) =>
   //   [...cars].sort((a, b) => {
   //     switch (order) {
   //       case 'asc':
   //         return a.make.localeCompare(b.make);
-  
+
   //       case 'desc':
   //         return b.make.localeCompare(a.make);
-  
+
   //       default:
   //         return 0;
   //     }
   //   });
-  
+
   // console.table(sortByMake(cars, 'asc'));
   // console.table(sortByMake(cars, 'desc'));
   // ====================================
   // Example 10 - Метод reduce
   // Пусть функция getTotalAmount возвращает общее
   //  количество автомобилей(значение свойств amount).
-  
+
   // const getTotalAmount = (cars) =>
   //   cars.reduce((acc, { amount }) => acc + amount, 0);
   // console.log(getTotalAmount(cars));
@@ -338,13 +360,12 @@ const cars = [
   // Пусть функция getAvailableCarNames возвращает
   //  массив моделей автомобилей, но только тех, которые
   //  сейчас на распродаже.
-  
+
   // const getModelsOnSale = (cars) =>
   //   cars.filter(({ onSale }) => onSale).map(({ model }) => model);
   // console.table(getModelsOnSale(cars));
-  
+
   // таблиця машин на які є скидка
   // const getModelsOnSale = (cars) =>
   //   cars.filter(({ onSale }) => onSale);
   // console.table(getModelsOnSale(cars));
-  
