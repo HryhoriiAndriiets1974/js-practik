8f5e23a3f805eb60f3dd99db1c5f933ffb1ca86a
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-05-13 10:51:25 +0300

    js-array-methods

---
+++ js/js-array-methods.js
@@ -0,0 +1,3 @@
+console.log('JS Array Methods');
+const atR = [1, 2, 3, 4, 5].at(1); console.log(`[1, 2, 3, 4, 5].at(1)  =>  ${atR}`);
+const contactR = [1, 2, 3, 4, 5].concat([4,5]); console.log(`[1, 2, 3, 4, 5].concat([4,5])  => [ ${contactR}]`);
