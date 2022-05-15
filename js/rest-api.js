b2bc252c1247b13555d45d05039e7ea453113830
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-05-15 13:13:29 +0300

    puplic-rest-api+posts

--- js/rest-api.js
+++ js/rest-api.js
@@ -1,5 +1,42 @@
 const fetchUsersBtn = document.querySelector(".btn");
+const fetchPostBtn = document.querySelector(".btn-post");
 const userList = document.querySelector(".user-list");
+
+// posts
+
+fetchPostBtn.addEventListener("click", () => {
+  fetchPosts()
+    .then((posts) => renderPostList(posts))
+    .catch((error) => console.log(error));
+});
+
+function fetchPosts() {
+  return fetch("https://jsonplaceholder.typicode.com/posts").then(
+    (response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    }
+  );
+}
+
+function renderPostList(posts) {
+  userList.innerHTML = "";
+  const markup = posts
+    .map((post) => {
+      return `<li>
+          <p><b>UserId</b>: ${post.userId}</p>
+          <p><b>Id</b>: ${post.id}</p>
+          <p><b>Title</b>: ${post.title}</p>
+          <p><b>Body</b>: ${post.body}</p>
+        </li>`;
+    })
+    .join("");
+  userList.innerHTML = markup;
+}
+
+// users
 
 fetchUsersBtn.addEventListener("click", () => {
   fetchUsers()
@@ -19,6 +56,7 @@ function fetchUsers() {
 }
 
 function renderUserList(users) {
+  userList.innerHTML = "";
   const markup = users
     .map((user) => {
       return `<li>
